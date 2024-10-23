0c5fa9ac074cadc4e79a68c9f644e342b53f3912
Rimsha-Ajmal <114993374+Rimsha-Ajmal@users.noreply.github.com>
2024-10-22 23:43:47 -0700

    contact table crud

---
+++ Backend/ContactApp/src/main/java/com/rimsha/ContactApp/service/ContactService.java
@@ -0,0 +1,78 @@
+package com.rimsha.ContactApp.service;
+
+import com.rimsha.ContactApp.dto.ContactDto;
+import com.rimsha.ContactApp.model.Contact;
+import com.rimsha.ContactApp.model.User;
+import com.rimsha.ContactApp.repo.ContactRepo;
+import com.rimsha.ContactApp.repo.UserRepo;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class ContactService {
+
+    @Autowired
+    private ContactRepo contactRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    public List<Contact> getContactsByUserId(String id) {
+        return contactRepo.findAllByUser_Id(id);
+    }
+
+    public Contact createContact(ContactDto contactDto) {
+        Contact contact = new Contact();
+        contact.setFirstName(contactDto.getFirstName());
+        contact.setLastName(contactDto.getLastName());
+        contact.setPhone(contactDto.getPhone());
+        contact.setEmail(contactDto.getEmail());
+        contact.setAddress(contactDto.getAddress());
+
+        User user = userRepo.findById(contactDto.getUser_id()).get();
+        contact.setUser(user);
+        contactRepo.save(contact);
+        return contact;
+    }
+
+    public Contact updateContact(ContactDto contactDto, String id) {
+        Contact updateContact = contactRepo.findById(id).get();
+
+        User user = userRepo.findById(contactDto.getUser_id()).get();
+        updateContact.setUser(user);
+
+        updateContact.setFirstName(contactDto.getFirstName());
+        updateContact.setLastName(contactDto.getLastName());
+        updateContact.setPhone(contactDto.getPhone());
+        updateContact.setEmail(contactDto.getEmail());
+        updateContact.setAddress(contactDto.getAddress());
+
+        contactRepo.save(updateContact);
+        return updateContact;
+    }
+
+    public Contact deleteContact(String id){
+        Contact deleteContact = contactRepo.findById(id).orElse(null);
+
+        if(deleteContact != null) {
+            Contact contact = deleteContact;
+            contact.setUser(null);
+            contactRepo.save(contact);
+            contactRepo.delete(contact);
+            return deleteContact;
+        }
+        return null;
+    }
+
+}
+
+
